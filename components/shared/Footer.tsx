f6f183f535343bb89cb76ff799df7a3bcda18c5f
Dante <jamesmidzi@gmail.com>
2022-04-10 14:16:34 +0200

    testing things

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -5,6 +5,7 @@ export default function Footer() {
   return (
     <footer className="flex items-center justify-center bg-gray-900 py-4">
       <SocialIcons />
+      <h3>Made by James Midsi | © {new Date().getFullYear()}</h3>
     </footer>
   );
 }
